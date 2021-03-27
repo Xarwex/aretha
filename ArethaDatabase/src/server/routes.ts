ba4ef1b4dc98f5ee7c730b5acfea50ed5d2eb3f4
Xarwex <oleksander1999@gmail.com>
2021-03-27 12:32:37 +0100

    ArethaDB api

--- ArethaDatabase/src/server/routes.ts
+++ ArethaDatabase/src/server/routes.ts
@@ -1,9 +1,20 @@
-import * as express from 'express';
+import * as express from 'express'
+import DB from './db'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/api/hello', (req, res, next) => {
     res.json('World');
 });
+
+router.get('/api/actions', async (req, res) => {
+    try {
+        let blogs = await DB.Actions.all()
+        res.json(blogs)
+    } catch(e) {
+        console.log(e)
+        res.sendStatus(500)
+    }
+})
 
 export default router;
