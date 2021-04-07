9539363db95f7bc922f657d3b88250dac579e74f
Xarwex <oleksander1999@gmail.com>
2021-04-07 13:01:45 +0200

    .

--- DemoApp-Aretha/src/client/App.tsx
+++ DemoApp-Aretha/src/client/App.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import arethaRegistryURL from './../../config/index'
 
-/* HOOK REACT EXAMPLE */
 const App = (props: AppProps) => {
 	const [appNameList, setAppNameList] = useState<JSON>(JSON.parse("{}"))
 
@@ -44,39 +43,5 @@ const App = (props: AppProps) => {
 };
 
 interface AppProps { }
-
-/* CLASS REACT EXAMPLE */
-// class App extends React.Component<IAppProps, IAppState> {
-// 	constructor(props: IAppProps) {
-// 		super(props);
-// 		this.state = {
-// 			name: null
-// 		};
-// 	}
-
-// 	async componentDidMount() {
-// 		try {
-// 			let r = await fetch('/api/hello');
-// 			let name = await r.json();
-// 			this.setState({ name });
-// 		} catch (error) {
-// 			console.log(error);
-// 		}
-// 	}
-
-// 	render() {
-// 		return (
-// 			<main className="container my-5">
-// 				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-// 			</main>
-// 		);
-// 	}
-// }
-
-// export interface IAppProps {}
-
-// export interface IAppState {
-// 	name: string;
-// }
 
 export default App;
